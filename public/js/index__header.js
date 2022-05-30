83162901ba6d41381449741eda5fec1594877def
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-05-30 10:53:04 +0700

    feat: add logout function for logout button

--- public/js/index__header.js
+++ public/js/index__header.js
@@ -4,6 +4,8 @@ window.addEventListener("DOMContentLoaded", function () {
     const userImage = document.getElementById("user__image");
     const userName = document.getElementById("user__name");
 
+    const logoutButon = document.getElementById("header__logout");
+
     const authenticatedUser = JSON.parse(localStorage.getItem("auth"));
 
     const showHeaderInformation = () => {
@@ -16,5 +18,22 @@ window.addEventListener("DOMContentLoaded", function () {
 
     if (authenticatedUser) {
         showHeaderInformation();
+
+        // add event for logout
+        if (logoutButon) {
+            logoutButon.addEventListener("click", function () {
+                const isLeaved = confirm("Do you want to log out?");
+                if (isLeaved) {
+                    // logout from cometchat and then clear storage.
+                    CometChat.logout().then((response) => {
+                        // User successfully logged out.
+                        // Perform any clean up if required.
+                        localStorage.removeItem("auth");
+                        // redirect to login page.
+                        window.location.href = "/logout";
+                    });
+                }
+            });
+        }
     }
 });
