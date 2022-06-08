8b835fe503ea2f22513b050032fc261c649109db
Phạm Bích Ngọc <20020454@vnu.edu.vn>
2022-06-08 15:50:55 +0700

    committ

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require("express-session");
 const path = require("path")
 
 
-// initialize body-parser to parse incoming parameters requests to req.bod
+// initialize body-parser to parse incoming parameters requests to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
